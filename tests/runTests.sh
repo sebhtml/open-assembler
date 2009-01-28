25aac6460d810d72e9df98b344032e03ce1b845c
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2009-01-28 21:21:56 +0000

    added hawkeye thing

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -4,8 +4,12 @@ for i in $(ls *.fa.fa)
 do
 	(
 	dna_DeBruijnAssembler  -assemblyDirectory D$i $i 
-	dna_Merger D$i/Contigs.fasta D$i/Assembly.fa > D$i/Merger.log
-	blat $(echo $i|sed 's/.fa$//') D$i/Assembly.fa D$i/blat.psl -fastMap 
-	blat $(echo $i|sed 's/.fa$//') D$i/Assembly.fa D$i/blat.blast -fastMap -out=blast
+	cp $i D$i/fasta.reads
+	tarchive2amos -s D$i/fasta.reads -o D$i/reads.afg
+	bank-transact  -m D$i/reads.afg -b D$i/bank -c
+	bank-transact -m D$i/Assembly.afg -b D$i/bank
+	#dna_Merger D$i/Contigs.fasta D$i/Assembly.fa > D$i/Merger.log
+	#blat $(echo $i|sed 's/.fa$//') D$i/Assembly.fa D$i/blat.psl -fastMap 
+	#blat $(echo $i|sed 's/.fa$//') D$i/Assembly.fa D$i/blat.blast -fastMap -out=blast
 	)&
 done
